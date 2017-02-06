1752d9bb2b1a802e6f77c8f384bf19afe5f30cfb
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-06 18:19:05 -0500

    basic index.html setup with seeded data and angular

--- public/app.js
+++ public/app.js
@@ -5,13 +5,20 @@ var app = angular.module('colorpalette', []);
 
 app.controller('mainController', ['$http', function($http) {
     this.message = "controller works";
-
+    var controller = this;
 
     $http({
         method: 'GET',
-        url: 'http://localhost:3000/palettes',
-    }).then(function(response) {
-        console.log(response);
-    });
+        url: "http://localhost:3000/palettes"
+    }).then(
+        function(res) {
+            console.log(controller);
+            console.log(res, " :successful callback");
+            controller.palettes = res.data;
+     },
+        function(res) {
+            console.log(res, " :failed callback");
+     }.bind(this));
+
 
 }]);
