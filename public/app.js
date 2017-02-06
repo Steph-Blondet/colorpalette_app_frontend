74e2224968a4d1cecb6cd41679f973f92a31ca1e
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-06 15:53:15 -0500

    setting up bootstrap

--- public/app.js
+++ public/app.js
@@ -4,6 +4,14 @@ console.log('color palette app connected');
 var app = angular.module('colorpalette', []);
 
 app.controller('mainController', ['$http', function($http) {
-    this.message = "controller works"
+    this.message = "controller works";
+
+
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3000/palettes',
+    }).then(function(response) {
+        console.log(response);
+    });
 
 }]);
