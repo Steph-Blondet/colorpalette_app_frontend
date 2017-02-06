81d0547d3d54633dd14935a7a92b32f634d7331f
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-06 15:40:09 -0500

    basic angular frontend setup

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
 console.log('color palette app connected');
+
+
+var app = angular.module('colorpalette', []);
+
+app.controller('mainController', ['$http', function($http) {
+    this.message = "controller works"
+
+}]);
