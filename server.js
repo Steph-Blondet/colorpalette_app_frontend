de23f4d48c07ce6a4fc4f9074ecc00fc737a40e7
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-11 12:51:53 -0500

    changing port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app     = express();
-var port    = 3001 || process.env.PORT;
+// var port    = 3001 || process.env.PORT;
 
 app.use(express.static('public'));
 
-app.listen(port, function() {
-  console.log("ColorPalette Frontend running on port: ", port);
-});
+// app.listen(port, function() {
+//   console.log("ColorPalette Frontend running on port: ", port);
+// });
+
+app.listen(process.env.PORT || 3001)
